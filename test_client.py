9ecf1044ed1c02998c01343cca40848eeafa5b75
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-05-08 10:51:40 -0400

    Asynchronous Client/Server Pattern

--- test_client.py
+++ test_client.py
@@ -3,22 +3,51 @@ import zmq
 
 
 class MyTestCase(unittest.TestCase):
-    def test_connection(self):
+    def test_client1(self):
         context = zmq.Context()
+        socket = context.socket(zmq.DEALER)
+        identity = u'worker-%d' % 1
+        socket.identity = identity.encode('ascii')
+        socket.connect('tcp://localhost:5555')
+        print('Client %s started' % identity)
+        poll = zmq.Poller()
+        poll.register(socket, zmq.POLLIN)
+        reqs = 0
+        while True:
+            reqs = reqs + 1
+            print('Req #%d sent..' % reqs)
+            socket.send(u'request #%d' % reqs)
+            for i in range(10):
+                sockets = dict(poll.poll(1000))
+                if socket in sockets:
+                    msg = socket.recv()
+                    print('Client %s received: %s' % (identity, msg))
 
-        #  Socket to talk to server
-        print("Connecting to hello world server…")
-        socket = context.socket(zmq.REQ)
-        socket.connect("tcp://localhost:5555")
+        socket.close()
+        context.term()
 
-        #  Do 10 requests, waiting each time for a response
-        for request in range(10):
-            print("Sending request %s …" % request)
-            socket.send(b"Hello")
+    def test_client2(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.DEALER)
+        identity = u'worker-%d' % 2
+        socket.identity = identity.encode('ascii')
+        socket.connect('tcp://localhost:5555')
+        print('Client %s started' % identity)
+        poll = zmq.Poller()
+        poll.register(socket, zmq.POLLIN)
+        reqs = 0
+        while True:
+            reqs = reqs + 1
+            print('Req #%d sent..' % reqs)
+            socket.send(u'request #%d' % reqs)
+            for i in range(10):
+                sockets = dict(poll.poll(1000))
+                if socket in sockets:
+                    msg = socket.recv()
+                    print('Client %s received: %s' % (identity, msg))
 
-            #  Get the reply.
-            message = socket.recv()
-            print("Received reply %s [ %s ]" % (request, message))
+        socket.close()
+        context.term()
 
 
 if __name__ == '__main__':
